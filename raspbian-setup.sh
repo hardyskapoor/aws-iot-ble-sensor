dc353e7c1eb6e99c8405cd8bc3bca1105bc58553
Konstantinos Konstantinidis <konstantinos.konstantinidis@ascential.com>
2016-04-30 20:09:04 +0100

    automatically install and setup supervisor

--- raspbian-setup.sh
+++ raspbian-setup.sh
@@ -7,23 +7,50 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+print_status() {
+    echo
+    echo "## $1"
+    echo
+}
+
+bail() {
+    echo 'Error executing command, exiting'
+    exit 1
+}
+
+exec_cmd_nobail() {
+    echo "+ $1"
+    bash -c "$1"
+}
+
+exec_cmd() {
+    exec_cmd_nobail "$1" || bail
+}
+
 # Install the NodeSource Node.js 6.x repo
 curl -sL https://deb.nodesource.com/setup_6.x | bash -
 
-# Upgrade all packages
-apt-get update ; apt-get upgrade -y
+print_status "Running apt-get upgrade..."
+exec_cmd 'apt-get upgrade -y'
 
-# Install Node.js, Git and other dependencies
-apt-get install -y nodejs git libudev-dev
+print_status "Installing Node.js, Supervisor, Git and other dependencies..."
+exec_cmd 'apt-get install -y nodejs supervisor git libudev-dev'
 
-# Install aws-iot-ble-sensor app
+print_status "Checking out aws-iot-ble-sensor..."
 cd /opt
-git clone https://github.com/kkonstan/aws-iot-ble-sensor.git
+exec_cmd 'git clone https://github.com/kkonstan/aws-iot-ble-sensor.git'
+
+print_status "Installing aws-iot-ble-sensor dependencies..."
 cd /opt/aws-iot-ble-sensor
-npm install
+exec_cmd 'npm install'
 
-# Prepare /boot/setup/certs (FAT on Raspbian) for the AWS IoT certs
+print_status "Preparing /boot/setup/certs for the AWS IoT certs..."
 mkdir -p /boot/setup/certs
-ln -s /boot/setup/certs /opt/aws-iot-ble-sensor/certs
+exec_cmd 'ln -s /boot/setup/certs /opt/aws-iot-ble-sensor/certs'
 
-echo "Don't forget to install the certificates in /boot/setup/certs"
+print_status "Setting up Supervisor to startup and monitor aws-iot-ble-sensor..."
+cat >/etc/supervisor/conf.d <<EOF
+[program:aws-iot-ble-sensor]
+directory=/opt/aws-iot-ble-sensor/
+command=/usr/bin/nodejs /opt/aws-iot-ble-sensor/aws-iot-ble-sensor.js -l
+EOF
