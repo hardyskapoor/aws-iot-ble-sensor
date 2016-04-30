3473dd817900cac80f61488a1611d9c4a98dc5b6
Konstantinos Konstantinidis <kkonstan@ianos.co.uk>
2016-04-30 20:28:04 +0100

    also move /etc/hostname to boot

--- raspbian-setup.sh
+++ raspbian-setup.sh
@@ -44,15 +44,20 @@ print_status "Installing aws-iot-ble-sensor dependencies..."
 cd /opt/aws-iot-ble-sensor
 exec_cmd 'npm install'
 
-print_status "Preparing /boot/setup/aws-iot-cert for the AWS IoT cert..."
+print_status "Preparing /boot/setup/aws-iot-cert/ for the AWS IoT cert..."
 mkdir -p /boot/setup/aws-iot-cert
 exec_cmd 'ln -s /boot/setup/aws-iot-cert /opt/aws-iot-ble-sensor/certs'
 
-print_status "Preparing /boot/setup/wifi for the WiFi config..."
+print_status "Preparing /boot/setup/wifi/ for the WiFi config..."
 mkdir -p /boot/setup/wifi
 touch /boot/setup/wifi/wpa_supplicant.conf 
 rm -rf /etc/wpa_supplicant/wpa_supplicant.conf
 exec_cmd 'ln -s /boot/setup/wifi/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf'
+
+print_status "Preparing /boot/setup/hostname for the hostname/clientId setup..."
+touch /boot/setup/hostname
+rm -rf /etc/hostname
+exec_cmd 'ln -s /boot/setup/hostname /etc/hostname'
 
 print_status "Setting up Supervisor to startup and monitor aws-iot-ble-sensor..."
 cat >/etc/supervisor/conf.d/aws-iot-ble-sensor.conf <<EOF
